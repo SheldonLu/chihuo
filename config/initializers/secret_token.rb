d8495958ce7a198b744cc7057dc5ab4fac27be6c
Administrator <ivan@ubuntu.ubuntu-domain>
2012-07-24 14:17:45 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chihuo::Application.config.secret_token = '2c4741527fe99f892a2a4c50093b449510e36ae84677f59fbff37cc5caa2ee5da7df3c8135bc1ace9d8d5b83c6de97bbdf96a8de1f244128f841da2ef3b91639'
